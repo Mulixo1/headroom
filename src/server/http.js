df7f76c1313e6a93724344eacc07f567ed34c8cb
mulixo <mustafa.ozen99@icloud.com>
2026-08-12 08:58:46 +0300

    Bump health version to 0.1.2

--- src/server/http.js
+++ src/server/http.js
@@ -223,7 +223,7 @@ export function createServer() {
         return sendJson(res, 200, {
           ok: true,
           service: "headroom",
-          version: "0.1.1",
+          version: "0.1.2",
         });
       }
 
